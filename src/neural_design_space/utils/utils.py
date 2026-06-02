45beba182c625f8ab229d110ffa57ec0e2f31ffc
linus <lagbleze@st.ug.edu.gh>
2026-06-02 20:42:20 +0200

    style: add docstring to kernel_initializer

--- src/neural_design_space/utils/utils.py
+++ src/neural_design_space/utils/utils.py
@@ -1,7 +1,14 @@
 import torch
 import torch.nn as nn
              
+           
 def kernel_initializer(m, kernel_initializer="he_normal"):
+    """Kernel initializer to be used on initializing Lazy modules, using He normal initialization for convolutional layers and Xavier uniform initialization for linear layers
+    Args:  m (nn.Module): module to initialize
+        kernel_initializer (str, optional): type of kernel initializer to use. Defaults to 'he_normal'. Options are 'he_normal' and 'glorot_uniform'
+    Returns:    
+        None
+    """
     if isinstance(m, nn.LazyConv2d) or isinstance(m, nn.LazyLinear) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
         if kernel_initializer == "he_normal":
             nn.init.kaiming_normal_(m.weight, mode="fan_in", nonlinearity="relu")
@@ -9,5 +16,3 @@ def kernel_initializer(m, kernel_initializer="he_normal"):
             nn.init.xavier_uniform_(m.weight)
         if m.bias is not None:
             nn.init.zeros_(m.bias)
-            
-            
