451c5eee0ad9ad09c832f2cf27b03981dd4c2225
linus <lagbleze@st.ug.edu.gh>
2026-06-14 23:53:37 +0200

    feat: add kernel_initializer

--- src/neural_design_space/models/densenet.py
+++ src/neural_design_space/models/densenet.py
@@ -1,6 +1,3 @@
-
-
-#%%
 import torch
 import torch.nn as nn
 from torchsummary import summary
@@ -8,7 +5,7 @@ from fvcore.nn import FlopCountAnalysis, get_bn_modules, flop_count_table, param
 from fvcore.nn.activation_count import ActivationCountAnalysis
 from torch.profiler import profile, record_function, ProfilerActivity
 from typing import NamedTuple
-
+from neural_design_space.utils.utils import kernel_initializer
 
 
 class StemDenseNet(nn.Module):
@@ -299,22 +296,6 @@ def make_model(num_classes, group_config, stem_out_channels=16, device="cuda"):
     model = nn.Sequential(stem, learner_module, classifier)
     return model.to(device)
     
-
-def kernel_initializer(m, kernel_initializer="he_normal"):
-    """Kernel initializer to be used on initializing Lazy modules, using He normal initialization for convolutional layers and Xavier uniform initialization for linear layers
-    Args:  m (nn.Module): module to initialize
-        kernel_initializer (str, optional): type of kernel initializer to use. Defaults to 'he_normal'. Options are 'he_normal' and 'glorot_uniform'
-    Returns:    
-        None
-    """
-    if isinstance(m, nn.LazyConv2d) or isinstance(m, nn.LazyLinear) or isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-        if kernel_initializer == "he_normal":
-            nn.init.kaiming_normal_(m.weight, mode="fan_in", nonlinearity="relu")
-        elif kernel_initializer == "glorot_uniform":
-            nn.init.xavier_uniform_(m.weight)
-        if m.bias is not None:
-            nn.init.zeros_(m.bias)
-                
                 
 if __name__ == "__main__":
     grp1 = DenseNetGroupConfig(n_blocks=6, growth_rate=32, compression=0.5, 
