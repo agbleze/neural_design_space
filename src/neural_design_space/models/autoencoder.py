9315bf82f0f90fdff948939fba3196ca540b94ad
linus <lagbleze@st.ug.edu.gh>
2026-06-17 23:00:58 +0200

    feat: add decoder

--- src/neural_design_space/models/autoencoder.py
+++ src/neural_design_space/models/autoencoder.py
@@ -16,11 +16,33 @@ class AutoEncoderStem(nn.Module):
             nn.LazyConv2d(out_channels=64, kernel_size=3, stride=2, padding=1, bias=False),
             nn.LazyBatchNorm2d(),
             nn.ReLU(),
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Flatten(),
-            nn.LazyLinear(out_features=latent_dim)
+            # nn.AdaptiveAvgPool2d((1, 1)),
+            # nn.Flatten(),
+            # nn.LazyLinear(out_features=latent_dim)
         )
         
     def forward(self, x):
         x = self.encoder(x)
+        return x
+    
+    
+class Decoder(nn.Module):
+    def __init__(self, latent_dim):
+        super().__init__()
+        self.pointwise_conv = nn.Sequential(
+            nn.LazyLinear(out_features=64),
+            nn.LazyBatchNorm1d(),
+            nn.ReLU()
+        )
+        self.decoder = nn.Sequential(
+            nn.LazyConvTranspose2d(out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1, bias=False),
+            nn.LazyBatchNorm2d(),
+            nn.ReLU(),
+            nn.LazyConvTranspose2d(out_channels=3, kernel_size=3, stride=2, padding=1, output_padding=1)
+        )
+        
+    def forward(self, x):
+        x = self.pointwise_conv(x)
+        x = x.view(x.size(0), 64, 1, 1)
+        x = self.decoder(x)
         return x
