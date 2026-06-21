2d19a0ecf354a269cb8f6c649a6b4045593f2c1f
linus <lagbleze@st.ug.edu.gh>
2026-06-21 23:43:05 +0200

    fix: change autoencoder

--- src/neural_design_space/models/autoencoder.py
+++ src/neural_design_space/models/autoencoder.py
@@ -7,19 +7,23 @@ from neural_design_space.utils.utils import kernel_initializer
 
 
 class Encoder(nn.Module):
-    def __init__(self, latent_dim):
+    def __init__(self, layers: list[int]):
         super().__init__()
-        self.encoder = nn.Sequential(
-            nn.LazyConv2d(out_channels=64, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU(),
-            nn.LazyConv2d(out_channels=32, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU(),
-            nn.LazyConv2d(out_channels=latent_dim, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU()
-        )
+        
+        encoder_layers = []
+        for out_ch in layers:
+            encoder_layer = nn.Sequential(nn.LazyConv2d(out_channels=out_ch, 
+                                                        kernel_size=3, stride=2, 
+                                                        padding=1, 
+                                                        bias=False
+                                                        ),
+                                          nn.LazyBatchNorm2d(),
+                                          nn.ReLU()
+                                          )
+            encoder_layers.append(encoder_layer)
+            self.add_module(f"encoder_layer_{out_ch}", encoder_layer)
+        self.encoder = nn.Sequential(*encoder_layers)
+        
         
     def forward(self, x):
         x = self.encoder(x)
@@ -27,27 +31,77 @@ class Encoder(nn.Module):
     
     
 class Decoder(nn.Module):
-    def __init__(self, latent_dim):
+    def __init__(self, layers: list[int]):
         super().__init__()
-        self.pointwise_conv = nn.Sequential(
-            nn.LazyConv2d(out_channels=64, kernel_size=1, bias=False),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU()
-        )
-        self.decoder = nn.Sequential(
-            nn.LazyConvTranspose2d(out_channels=32, kernel_size=3, stride=2, padding=1, output_padding=1, bias=False),
-            nn.LazyBatchNorm2d(),
-            nn.ReLU(),
-            nn.LazyConvTranspose2d(out_channels=3, kernel_size=3, stride=2, padding=1, output_padding=1)
-        )
+        decoder_layers = []
+        decoder_layers_out_ch = layers[::-1]
+        
+        for out_ch in decoder_layers_out_ch:
+            decoder_layer = nn.Sequential(nn.LazyConvTranspose2d(out_channels=out_ch,
+                                                                 kernel_size=3, stride=2, padding=1, 
+                                                                 output_padding=1, 
+                                                                 bias=False
+                                                                 ),
+                                        nn.LazyBatchNorm2d(), 
+                                        nn.ReLU()
+                                        )
+            decoder_layers.append(decoder_layer)
+        self.decoder = nn.Sequential(*decoder_layers)
+        #self.decoder_activation = nn.Sigmoid()
         
     def forward(self, x):
-        x = self.pointwise_conv(x)
-        x = x.view(x.size(0), 64, 1, 1)
         x = self.decoder(x)
+        #x = self.decoder_activation(x)
+        return x
+    
+
+class ReconstructTask(nn.Module):
+    def __init__(self, out_channels):
+        super().__init__()
+        self.task_layer = nn.Sequential(nn.LazyConvTranspose2d(out_channels=out_channels,
+                                                               kernel_size=3, stride=2, padding=1, 
+                                                              output_padding=1, bias=False
+                                                              ),
+                                        nn.Sigmoid()
+                                        )
+        
+    def forward(self, x):
+        x = self.task_layer(x)
         return x
     
     
+    
+class AutoEncoder(nn.Module):
+    def __init__(self, layer_channels: list[int], out_channels=3):
+        super().__init__()
+        self.encoder = Encoder(layer_channels)
+        self.decoder = Decoder(layer_channels)
+        self.reconstruct_task = ReconstructTask(out_channels)
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        x = self.reconstruct_task(x)
+        return x
 
-def build_model():
-    pass
+
+
+
+def make_model(layer_channels, data, device="cuda", 
+               initializer_type="he_normal"
+               ):
+    out_channels = data.shape[1]
+    model = AutoEncoder(layer_channels=layer_channels, out_channels=out_channels).to(device)
+    data.to(device)
+    _ = model(data)
+    model.apply(lambda module: kernel_initializer(module, initializer_type=initializer_type))
+    return model
+
+
+data = torch.randn(1, 3, 224, 224)
+layer_channels = [64, 128, 256, 512]
+model = make_model(layer_channels=layer_channels, data=data, device="cuda", initializer_type="he_normal")
+summary(model, input_size=data.shape)
+
+
+
